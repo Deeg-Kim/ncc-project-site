815e34a164c7f45eb3f5d02b850d7e46477a7b2b
DG Kim <donggyunkim@gmail.com>
2018-04-20 01:49:58 -0400

    AJAX search

--- plugins/dgkim/digitalresourcesdb/components/ResourcesList.php
+++ plugins/dgkim/digitalresourcesdb/components/ResourcesList.php
@@ -27,8 +27,90 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 	
 	public function onRun()
 	{
-		$resources = Resource::get();
-		
-		$this->page['resources'] = $resources;
+		$this->prepareVars(); 
 	}
+	
+	function onFilter() 
+	{ 
+		$this->prepareVars(); 
+	}
+	
+	function prepareVars() {
+		function array_strpos($haystack, $needles)
+		{
+			foreach($needles as $needle)
+				if(strpos($haystack, $needle) !== false) return true;
+			return false;
+		}
+		
+        $words = post('search');
+		$words = explode(' ', $words);
+		$words = array_map('strtolower', $words);
+		$searchType = post('searchType');
+		
+		$resources = Resource::get();
+		$resourceArray = [];
+		
+		foreach ($resources as $resource) {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $resource->link);
+			curl_setopt($ch, CURLOPT_HEADER, 1);
+			curl_setopt($ch , CURLOPT_RETURNTRANSFER, 1);
+			$data = curl_exec($ch);
+			$headers = curl_getinfo($ch);
+			curl_close($ch);
+
+			$status = $headers['http_code'];
+
+			$attributes = $resource->attributes;
+
+			if ($status == 200 || $status == 301 || $status == 302) {
+				$attributes['broken'] = 0;
+			} else {
+				$attributes['broken'] = 1;
+			}
+			
+			$attributes['categories'] = $resource->categories;
+			
+			if (empty($searchType)) {
+				$resourceArray[] = $attributes;
+			} elseif($words[0] == "") {
+				$resourceArray[] = $attributes;
+			} else {
+				
+				$added = false;
+				if (in_array("title", $searchType) && $added == false) {
+					$english = explode(' ', $attributes['name_english']);
+					$english = array_map('strtolower', $english);
+					
+					if(!empty(array_intersect($words, explode(' ', $attributes['name_japanese']))) || !empty(array_intersect($words, $english))) {
+						$resourceArray[] = $attributes;
+						$added = true;
+					}
+				}
+				
+				if (in_array("keywords", $searchType) && $added == false) {
+					$keywords = explode(',', $attributes['keywords']);
+					$keywords = array_map('strtolower', $keywords);
+					
+					if(!empty(array_intersect($keywords, $words))) {
+						$resourceArray[] = $attributes;
+						$added = true;
+					}
+				}
+				
+				if (in_array("description", $searchType) && $added == false) {
+					$description = strip_tags(strtolower($attributes['description']));
+					$flag = array_strpos($description, $words);
+					
+					if ($flag) {
+						$resourceArray[] = $attributes;
+						$added = true;
+					}
+				}
+			}
+		}
+		
+		$this->page['resources'] = $resourceArray;
+    }
 }
