f9c323d982dd96ad7941a1dc3e36cf1627829829
DG Kim <donggyunkim@gmail.com>
2018-08-23 21:17:52 -0700

    Hit back on resource view returns to search results/correct page

--- plugins/dgkim/digitalresourcesdb/components/ResourceView.php
+++ plugins/dgkim/digitalresourcesdb/components/ResourceView.php
@@ -5,18 +5,19 @@ use Input;
 use Validator;
 use Redirect;
 use Flash;
+use Session;
 use Cms\Classes\ComponentBase;
 use Cms\Classes\Page;
 use Dgkim\DigitalResourcesDb\Models\Resource;
 
 class ResourceView extends \Cms\Classes\ComponentBase
-{	
+{
 
     /**
      * Validation rules
      */
     public $rules = [];
-	
+
     public function componentDetails()
     {
         return [
@@ -24,11 +25,13 @@ class ResourceView extends \Cms\Classes\ComponentBase
             'description' => 'View resource from ID'
         ];
     }
-	
+
 	public function onRun()
 	{
 		$resource = Resource::where('id', $this->param('id'))->first();
-		
+        $pagedata = Session::get('page.data');
+
+        $this->page['pagedata'] = $pagedata;
 		$this->page['resource'] = $resource;
 	}
 }
--- plugins/dgkim/digitalresourcesdb/components/ResourcesList.php
+++ plugins/dgkim/digitalresourcesdb/components/ResourcesList.php
@@ -14,13 +14,13 @@ use Dgkim\DigitalResourcesDb\Models\Category;
 use Dgkim\DigitalResourcesDb\Models\Resource;
 
 class ResourcesList extends \Cms\Classes\ComponentBase
-{	
+{
 
     /**
      * Validation rules
      */
     public $rules = [];
-	
+
     public function componentDetails()
     {
         return [
@@ -34,17 +34,17 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 		list($usec, $sec) = explode(" ", microtime());
 		return ((float)$usec + (float)$sec);
 	}
-	
+
 	public function onRun()
 	{
-		$this->prepareVars(); 
+		$this->prepareVars();
 	}
-	
-	function onFilter() 
-	{ 
-		$this->prepareVars(); 
+
+	function onFilter()
+	{
+		$this->prepareVars();
 	}
-	
+
 	function prepareVars() {
 		function array_strpos($haystack, $needles)
 		{
@@ -52,11 +52,10 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 				if(strpos($haystack, $needle) !== false) return true;
 			return false;
 		}
-		
+
 		$categories = Category::orderBy('name_english')->get();
 
 		$alpha = get('r');
-		
 		if (get('st') == '' || post('submit')) {
         	$words = post('search');
 			$offset = get('page');
@@ -68,31 +67,31 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 			$searchType = unserialize(get('st'));
 			$categoryFilter = unserialize(get('cf'));
 		}
+        $q = $words;
 		if (post('submit')) {
 			$offset = 1;
 		}
-		
+
 		$full = $words;
 		$words = explode(' ', $words);
 		$words = array_map('strtolower', $words);
-		$minutes = 60;
-		$in_page = 10; // number of entries per page of each romanization
-		
+		$in_page = 1; // number of entries per page of each romanization
+
 		if ($alpha == null) {
 			$alpha = 'A';
 		}
-		
+
 		if ($offset == null) {
 			$offset = 1;
 		}
-		
+
 		if (empty($categoryFilter)) {
 			$categoryFilter = [];
 		}
-		
+
 		$categoryCount = count($categories);
 		$filterCount = count($categoryFilter);
-		
+
 		if (!empty($words[0]) || (($filterCount != 0) && ($categoryCount != $filterCount))) {
 			$searched = true;
 			$resources = Resource::orderBy('name_romanization')->get();
@@ -100,7 +99,7 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 			$searched = false;
 			$count = Resource::where('name_romanization', 'like', $alpha . '%')->count();
 			$page_count = ceil($count/$in_page);
-			
+
 			$resources = Resource::where('name_romanization', 'like', $alpha . '%')
 				->orderBy('name_romanization')
 				->skip(($offset - 1) * $in_page)
@@ -108,19 +107,19 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 				->get();
 		}
 		$resourceArray = [];
-		
+
 		foreach ($resources as $resource) {
-			
+
 			$attributes = $resource->attributes;
-			
+
 			$attributes['categories'] = $resource->categories;
-			
+
 			// build description blurbs
 			if ($resource->description != "") {
 				$str = $resource->description;
 				// preg_match('/<p>(.*?)<\/p>/i', $str, $paragraphs);
 				$paragraphs = preg_split('/<\/\s*p\s*>/', $str);
-					
+
 				if (!empty($paragraphs[1])) {
 					if (strlen(strip_tags($paragraphs[0])) > 150) {
 						$attributes['description_blurb'] = $paragraphs[0];
@@ -131,14 +130,14 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 					$attributes['description_blurb'] = $paragraphs[0];
 				}
 			} else {
-				$attributes['description_blurb'] = "";	
+				$attributes['description_blurb'] = "";
 			}
-			
+
 			if ($resource->description_japanese != "") {
 				$str = $resource->description_japanese;
 				// preg_match('/<p>(.*?)<\/p>/i', $str, $paragraphs);
 				$paragraphs = preg_split('/<\/\s*p\s*>/', $str);
-				
+
 				if (!empty($paragraphs[1])) {
 					if (strlen(strip_tags($paragraphs[0])) > 150) {
 						$attributes['description_japanese_blurb'] = $paragraphs[0];
@@ -149,18 +148,18 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 					$attributes['description_japanese_blurb'] = $paragraphs[0];
 				}
 			} else {
-				$attributes['description_japanese_blurb'] = "";	
+				$attributes['description_japanese_blurb'] = "";
 			}
-			
+
 			$categoryIds = [];
-			
+
 			foreach ($attributes['categories'] as $c) {
 				$categoryIds[] = $c->attributes['id'];
 			}
-			
+
 			// add if in category
 			if (!empty(array_intersect($categoryIds, $categoryFilter)) || empty($categoryFilter)) {
-				
+
 				// perform search on text input and on search fields
 				if (empty($searchType)) {
 					$resourceArray[] = $attributes;
@@ -170,30 +169,30 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 
 					$added = false;
 					if (in_array("title", $searchType) && $added == false) {
-						
+
 						$flag = array_strpos(strtolower($attributes['name_english']), $words);
-							
+
 						if ($added == false && $flag == true) {
 							$resourceArray[] = $attributes;
 							$added = true;
 						}
-							
+
 						$flag = array_strpos(strtolower($attributes['name_romanization']), $words);
-							
+
 						if ($added == false && $flag == true) {
 							$resourceArray[] = $attributes;
 							$added = true;
 						}
-							
+
 						$flag = array_strpos($attributes['name_japanese'], $words);
-							
+
 						if ($added == false && $flag == true) {
 							$resourceArray[] = $attributes;
 							$added = true;
 						}
-					
+
 					}
-					
+
 					if (in_array("keywords", $searchType) && $added == false) {
 						$keywords = explode(',', $attributes['keywords']);
 						$keywords = array_map('strtolower', $keywords);
@@ -212,7 +211,7 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 							$resourceArray[] = $attributes;
 							$added = true;
 						}
-						
+
 						if ($added == false) {
 							$description_japanese = strip_tags($attributes['description_japanese']);
 							$flag_jap = array_strpos($description_japanese, $words);
@@ -223,49 +222,49 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 							}
 						}
 					}
-					
+
 				}
 			}
 		}
-		
+
 		$ab = range('A', 'Z');
 		$pagination = [];
-		
+
 		foreach ($ab as $a) {
 			$resources = Resource::where('name_romanization', 'like', $a . '%')->get();
 			$c = count($resources);
-			
+
 			if ($c == 0) {
 				$has = 0;
 			} else {
 				$has = 1;
 			}
-			
+
 			if ($a == $alpha) {
 				$active = 1;
 			} else {
 				$active = 0;
 			}
-			
+
 			$pagination[] = array(
 				'letter'	=> $a,
 				'has'		=> $has,
 				'active'	=> $active
 			);
 		}
-		
+
 		$this->page['resourceCount'] = count($resourceArray);
-		
+
 		if ($searched) {
 			$count = count($resourceArray);
 			$page_count = ceil($count/$in_page);
-			
+
 			$resources = Resource::where('name_romanization', 'like', $alpha . '%')
 				->orderBy('name_romanization')
 				->skip(($offset - 1) * $in_page)
 				->limit($in_page)
 				->get();
-			
+
 			if (!empty($resourceArray)) {
 				$paginatedArray = array();
 				$total = count($resourceArray);
@@ -277,20 +276,25 @@ class ResourcesList extends \Cms\Classes\ComponentBase
 
 				$resourceArray = $paginatedArray;
 			}
-		}
-		
+
+            $page_data = ['searched' => true, 'q' => $q, 'page' => $offset, 'st' => serialize($searchType), 'cf' => serialize($categoryFilter)];
+		} else {
+            $page_data = ['searched' => false, 'r' => $alpha, 'page' => $offset];
+        }
+        Session::flash('page.data', $page_data);
+
 		$this->page['pagination'] = $pagination;
 		$this->page['categories'] = $categories;
 		$this->page['resources'] = $resourceArray;
-		
+
 		$this->page['searched'] = $searched;
-		
+
 		if ($searched) {
 			$this->page['words'] = $full;
 			$this->page['searchType'] = serialize($searchType);
 			$this->page['categoryFilter'] = serialize($categoryFilter);
 		}
-		
+
 		$this->page['pageCount'] = $page_count;
 		$this->page['offset'] = $offset;
 		$this->page['alpha'] = $alpha;
